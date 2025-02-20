6ab16a8bb7a88e6e0488a99ed0c786e7937a2fab
PantelisGavalas <pantelis.g7@gmail.com>
2025-02-20 12:53:32 +0100

    Building a controller.

--- Exercise Files/04_02_begin/landon-hotel/src/main/java/com/pantelisgavalas/lil/landon_hotel/LandonHotelApplication.java
+++ Exercise Files/04_02_begin/landon-hotel/src/main/java/com/pantelisgavalas/lil/landon_hotel/LandonHotelApplication.java
@@ -1,4 +1,4 @@
-package com.frankmoley.lil.landonhotel;
+package com.pantelisgavalas.lil.landon_hotel;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class LandonHotelApplication {
 
-  public static void main(String[] args) {
-    SpringApplication.run(LandonHotelApplication.class, args);
-  }
+	public static void main(String[] args) {
+		SpringApplication.run(LandonHotelApplication.class, args);
+	}
 
 }
